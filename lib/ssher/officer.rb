ab6c44994cba4e2cd89d2daa95f353dffadf5f3c
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-07-06 17:36:54 -0700

    officer has list of callbacks instead of one

--- lib/ssher/officer.rb
+++ lib/ssher/officer.rb
@@ -2,13 +2,16 @@ module SSHer
   class Officer
     attr_reader :command
   
-    def initialize(command, callback)
+    def initialize(command, *callbacks)
       @command = command
-      @callback = callback
+      @callbacks = callbacks
     end
   
     def call(data)
-      @output = @callback.call(data)
+      #@output = @callback.call(data)
+      @output = @callbacks.inject([]) do |output, callback|
+        callback.call(data)
+      end
     end
   
     def report
